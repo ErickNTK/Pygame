3d4ecd42ba24847a8fdafffbe7ba74637a49e9d0
ErickTakenaka <erick.nodomi@gmail.com>
2024-10-10 15:51:26 -0300

    Create FinalGame.py

---
+++ FinalGame.py
@@ -0,0 +1,26 @@
+import pygame
+import pygame as pg
+
+pg.init()
+
+WIDTH = 800
+HEIGHT = 600
+WINDOW_SIZE = (WIDTH,HEIGHT)
+screen = pg.display.set_mode((WINDOW_SIZE))
+
+pg.display.set_caption("Game")
+
+FPS = 60
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+
+while True:
+    for event in pg.event.get():
+     if event.type == pg.QUIT:
+        exit()
+    screen.fill(WHITE)
